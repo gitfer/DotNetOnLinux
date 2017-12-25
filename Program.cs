82e69d34a721506c17631fc90fa7c9a71360c150
Federico Rota <federico.rota01@gmail.com>
2017-12-25 14:47:41 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DotNetOnLinux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	Console.WriteLine()
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
